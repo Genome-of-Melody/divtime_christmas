b8a295d2e29e717d9d88a18cce1a78973d2eff73
gaballench <gaballench@gmail.com>
2024-03-27 13:35:26 -0300

    Restructuring divtime

--- 01_divtime/code/00_calculate_maxcredtree.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# combine the tree samples
-pxlog -t ../../00*/analysis/*.run*.t -b 500 | pxt2new -o ../data/combined_trees.newick
-rm phyx.logfile
-
-treeannotator -file ../data/combined_trees.newick ../data/maxcredtree.tre
