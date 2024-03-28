1cc4e0928b0867d62139e9c9933f214db426b3c6
gaballench <gaballench@gmail.com>
2024-03-28 00:14:59 -0300

    Adding code for doing model selection using stepping stones. Including results which too roughly one hour on my laptop to finish for 25 rooted trees

---
+++ 01_model_selection/code/03_run_stepping_stones.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+cd ../analysis
+
+for i in {0..24}; do
+    mkdir tree$i
+    cd tree$i
+    cp ../../code/stepping_stones.mb ./
+    sed -e "s/TREEN/tree$i/g" -i stepping_stones.mb
+    ln -s ../../data/alignment_and_trees.nexus alignment_and_trees.nexus
+    mpirun -n 12 --oversubscribe mrbayes_volpiano-mpi stepping_stones.mb
+    cd ..
+done
+
