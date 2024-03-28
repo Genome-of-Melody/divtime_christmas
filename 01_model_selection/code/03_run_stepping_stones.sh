5cc10ca8dd7cd81ba80a9e7e03f66c2028b7825a
gaballench <gaballench@gmail.com>
2024-03-28 17:06:22 -0300

    Repeated the stepping stones this time with five independent runs in order to have better mean marginal loglik values for the trees. Still tree11 and tree19 are the best ones

--- 01_model_selection/code/03_run_stepping_stones.sh
+++ 01_model_selection/code/03_run_stepping_stones.sh
@@ -8,7 +8,7 @@ for i in {0..24}; do
     cp ../../code/stepping_stones.mb ./
     sed -e "s/TREEN/tree$i/g" -i stepping_stones.mb
     ln -s ../../data/alignment_and_trees.nexus alignment_and_trees.nexus
-    mpirun -n 12 --oversubscribe mrbayes_volpiano-mpi stepping_stones.mb
+    mpirun -n 20 --oversubscribe mrbayes_volpiano-mpi stepping_stones.mb
     cd ..
 done
 
