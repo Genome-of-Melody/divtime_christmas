4a5509c4496f064d747fcda35e3b3ecb84659e7f
gaballench <gaballench@gmail.com>
2024-04-11 00:34:29 -0300

    Finished divtime after almost a day setting up rates and mcmc sampling parameters. This analysis is quite brittle using MrBayes.

--- 02_divtime/code/00_run_mcmc_sampling.sh
+++ 02_divtime/code/00_run_mcmc_sampling.sh
@@ -2,8 +2,9 @@
 
 cd ../analysis
 
-for i in {11,19}; do
-#for i in '19'; do
+# uncomment if divtime should be estimated on more than one tree
+#for i in {tree,numbers,separated,by,commas}; do
+for i in '7'; do
     mkdir tree$i
     cd tree$i
     # sample from prior
@@ -13,7 +14,7 @@ for i in {11,19}; do
     sed -e "s/TREEN/tree$i/g" -i mcmc_sampling.mb
     sed -e "s/TOGGLEDATA/no/g" -i mcmc_sampling.mb
     ln -s ../../../../01_model_selection/data/alignment_and_trees.nexus alignment_and_trees.nexus
-    mpirun -n 20 --oversubscribe mrbayes_volpiano-mpi mcmc_sampling.mb
+    mpirun -n 8 --oversubscribe mrbayes_volpiano-mpi mcmc_sampling.mb
     cd ..
     # sample from posterior
     mkdir posterior
@@ -22,7 +23,7 @@ for i in {11,19}; do
     sed -e "s/TREEN/tree$i/g" -i mcmc_sampling.mb
     sed -e "s/TOGGLEDATA/yes/g" -i mcmc_sampling.mb
     ln -s ../../../../01_model_selection/data/alignment_and_trees.nexus alignment_and_trees.nexus
-    mpirun -n 20 --oversubscribe mrbayes_volpiano-mpi mcmc_sampling.mb
+    mpirun -n 8 --oversubscribe mrbayes_volpiano-mpi mcmc_sampling.mb
     cd ../..
 done
 
